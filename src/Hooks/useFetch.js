58ee0e489d3fe340522c666a7cdfb496f60e59f8
nidhi18-sharma <nidhis0418@gmail.com>
2026-06-01 00:37:18 +0530

    new changes

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,19 +1,28 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
+export default function useFetch() {
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState([]);
 
-function useFetch(){
-    useEffect(() => {
-        async function fetchUserSavedPlaces() {
-          setIsFetching(true);
-          try {
-            const places = await fetchUserPlaces();
-            setUserPlaces(places);
-          } catch (error) {
-            setErrorUserPlaces({
-              message: error.message || "Unable to fetch Saved Places",
-            });
-          }
-          setIsFetching(false);
-        }
-        fetchUserSavedPlaces();
-      }, []);
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const places = await fetchFn();
+        setFetchedData(places);
+      } catch (error) {
+        setError({
+          message: error.message || "Unable to fetch Data",
+        });
+      }
+      setIsFetching(false);
+    }
+    fetchData();
+  }, [fetchFn]);
+  return{
+    isFetching,
+    error,
+    fetchedData
+
+  }
 }
